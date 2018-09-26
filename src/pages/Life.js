0e7f7ee71e50c892f552b8bcee7cef248c6812f2
liumeichen <liumeichen_bupt@163.com>
2018-09-26 15:34:24 +0800

    feat：新增目录

---
+++ src/pages/Life.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+import './index.less'
+
+export default class Life extends React.Component {
+  render() {
+    return <div className="container">
+      <p>123123123</p>
+      <Button>点击</Button>
+    </div>
+  }
+}
